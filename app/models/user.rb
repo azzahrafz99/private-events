a2f8b9f4a174c2d24ff4257efa61bab73f26550b
Fatimah Az-Zahra <azzahrafz@gmail.com>
2017-06-09 15:49:29 +0700

    Adding following relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   validates :password_confirmation, presence:true
 
   has_many :events
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_secure_password
 
@@ -17,6 +25,20 @@ class User < ApplicationRecord
 
   def User.digest(token)
       Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  # follows as user
+  def follow(other_user)
+    following << other_user
+  end
+
+  # unfollows as user
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
